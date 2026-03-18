b0603dfe0fb084c607675752c278b1289719806e
AdyantDas <72867628+AdyantDas@users.noreply.github.com>
2026-03-18 10:00:10 +0530

    Update HelloApp to greet user by name

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,13 +1,9 @@
 import java.util.Scanner;
 public class HelloApp {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World");
-=======
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         System.out.println("Hello," + name + "!");
         sc.close();
->>>>>>> feature/UC2-display-name
     }
 }
